53ffdb3d761ceb574353cdadc38696209be0a0f2
ErrabelliSathvik <sathvikerrabelli16@gmail.com>
2026-04-09 12:34:02 +0530

    Added architecture diagram

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,27 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Delete, Param } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './create-user.dto';   // ✅ // ✅ IMPORT MUST BE HERE (TOP)
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post()
-  create(@Body() body: CreateUserDto) {
+  create(@Body() body: any) {
     return this.userService.createUser(body.name, body.email);
   }
 
   @Get()
   findAll() {
     return this.userService.getUsers();
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return this.userService.updateUser(Number(id), body.name, body.email);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.deleteUser(Number(id));
   }
 }
